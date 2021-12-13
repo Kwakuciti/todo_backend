5a12f7b440f1c9b402e1cee9a042575657a929bd
kwakuciti <kwekuciti@gmail.com>
2021-12-13 12:02:43 +0000

    todoschemas,index.js,env,.gitignore

---
+++ index.js
@@ -0,0 +1,89 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import TodoModel from './schemas/todo_schemas.js';
+
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+const db = process.env.DB_URL;
+const PORT = process.env.PORT || 3000
+
+mongoose.connect(db,{
+    useUnifiedTopology: true
+}).then(()=> {console.log('Connected to MongoDB successfully')}).catch((err)=> {
+    console.log(err)})
+
+
+// create a Todo
+app.post('/todos',async (req,res)=>{
+   try {
+       const newTodo =await TodoModel.create({...req.body})
+         res.status(201).json({
+             status: true,
+             message: 'Todo created successfully',
+             data: newTodo
+         })}
+         catch (error) {
+             console.log('Something went wrong',error);
+             //res.status(400).send('failed to fetch todo', error)
+
+         }
+            // get all todos
+            app.get('/todos',async(req,res)=>{
+                return res.status(200).json({
+
+                })
+            })
+        })
+
+
+ // update  Todo
+ app.patch('/todos/:id', async(req,res)=>{
+    try{
+        const{id} = req.params;
+        const{status} = req.body;
+
+
+        const updateTodo = await TodoModel.updateOne({status: status}).where({_id:id})
+        return res.status(201).json({
+            status: true,
+            message: 'Todo updated Successfull',
+            data: updateTodo
+        })
+    } catch  (err) {
+    console.log('Something went wrong', error)
+    }
+})
+
+     //delete a todo
+     app.delete('/todos/:id',async(req,res)=>{
+         try {
+             const {id} = re.params;
+             const deleteTodo = await TodoModel.findbyIdAndDelete(id);
+             return res.status(201).json({
+                 message: 'Todo dele successfully'
+             })
+
+         } catch (error) {
+             console.log('Something went wrong',error);
+         }
+     }
+     )
+
+
+    app.listen(PORT)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ schemas/todo_schemas.js
@@ -0,0 +1,25 @@
+import  mongoose  from "mongoose";
+const {Schema, model} = mongoose;
+
+const todoSchema = Schema({
+    title:{
+        type:String,
+        require: true
+    },
+    description:{
+        type:String,
+        require: true
+    },
+    date_time:{
+        type:String,
+        require: true
+    },
+    status:{
+        type: Boolean,
+        require: true,
+        default: false
+    },
+})
+
+  const todoModel = mongoose.model('todos',todoSchema)
+  export default todoModel;
